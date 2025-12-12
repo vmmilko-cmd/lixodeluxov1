f918785a70699bb93746cd214bbc5bf9ec1e5e52
vmmilko-cmd <vmmilko@gmail.com>
2025-12-12 18:47:47 +0100

    Update ProductDetail.tsx

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,1 +1,11 @@
+import React from 'react';
+
+export const ProductDetail: React.FC = () => {
+  return (
+    <div>
+      <h1>Detalle del producto</h1>
+      <p>Aquí puedes mostrar la información de un producto específico.</p>
+    </div>
+  );
+};
 
